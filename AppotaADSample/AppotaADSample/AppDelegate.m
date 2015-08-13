678498ba2f2b1d553c8223d830e37dc9dc1dd801
Tue Nguyen <tuent@appota.com>
2015-08-13 15:55:53 +0700

    Change becomeActive to auto

--- AppotaADSample/AppotaADSample/AppDelegate.m
+++ AppotaADSample/AppotaADSample/AppDelegate.m
@@ -25,7 +25,6 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    [AppotaADSDK resignActive];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -39,7 +38,6 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    [AppotaADSDK becomeActive];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
